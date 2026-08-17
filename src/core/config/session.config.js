77e7f3b3a294a58fb3510e89aa399088e4037946
Youssef Mahrous <youssefmahrous667@gmail.com>
2026-08-17 13:17:57 +0300

    Update session.config.js

--- src/core/config/session.config.js
+++ src/core/config/session.config.js
@@ -18,7 +18,7 @@ const sessionMiddleware = session({
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1000 * 60 * 60 * 24,
-    sameSite: 'lax'
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
   }
 });
 
